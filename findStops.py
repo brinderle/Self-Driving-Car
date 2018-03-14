18b3a2cba55917cded5c1f395cffe7063e70837e
brinderle <32775075+brinderle@users.noreply.github.com>
2018-03-13 23:43:25 -0400

    multiple longitude functionality, stored stops
    allowed for one latitude to corresponds to multiple longitude coordinates, also stored all of the stop coordinates in a list so that they can be accessed more easily than the dictionary

--- findStops.py
+++ findStops.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-EvanData1 = pd.read_csv("C:/Users/student/Documents/CS4630/Self-Driving Project/EvanData1.csv")
+EvanData1 = pd.read_csv("C:/Users/student/Documents/CS4630/Self-Driving Project/Data02-24_13-56.csv")
 EvanData1Subset = EvanData1.iloc[:,[1,5,6,7,8,21,22]]
 # maybe add rotation column
 EvanData1Subset.columns = ["sampleNum", "latitude", "longitude", "altitude", "speed", "accelerationX", "accelerationY"]
@@ -18,8 +18,19 @@ for i in range(len(EvanData1Subset)):
     accelY = EvanData1Subset.iloc[i,6]
 
     # check if speed is less than 1.5 m/s (3.36 mph) and then something about accelerationY
+    # also need to allow for one latitude with multiple longitudes, which is why values for dictionary keys are lists
     if speed < 1.5 and accelY > .1 and latitude not in stops:
-        stops[str(latitude)] = longitude
+        stops[str(latitude)] = [longitude]
+    elif speed < 1.5 and accelY > .1 and latitude in stops:
+        stops[str(latitude)] = stops[str(latitude)].append(longitude)
+
+# this is a list of list of coordinate pairs for the stops, can be used for the database
+stopCoordinateList = []
 
 for i in stops:
-    print(i, ", ", stops[i], sep="")
+    # this inner loop goes through each item in the list, which is the dictionary value
+    # this allows for one latitude with multiple longitudes to be treated properly
+    for j in range(len(stops[i])):
+        stopCoordinateList.append([i,stops[i][j]])
+    # stopCoordinateList.append([i,stops[i]])
+        print(i, ", ", stops[i][j], sep="")
